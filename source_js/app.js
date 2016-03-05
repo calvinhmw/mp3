2f3759c8370be36c30b182f21ca607d4a6736707
Mingwei Hu <calvinmwhu@gmail.com>
2016-03-05 01:38:29 -0600

    finished list view

--- source_js/app.js
+++ source_js/app.js
@@ -1,5 +1,11 @@
 var app = angular.module('mp3',['ngRoute']);
 
 app.config(function ($routeProvider) {
-
-})
+    $routeProvider
+        .when('/list', {
+            templateUrl: 'partials/list.html',
+            controller: 'ListViewController'
+        }).otherwise({
+       redirectTo: '/list'
+    });
+});
--- source_js/controllers.js
+++ source_js/controllers.js
@@ -1,1 +1,10 @@
-/* Sample Controller */
+app.controller('ListViewController', ['$scope', '$http', function ($scope, $http) {
+    $scope.header = 'Search for a movie here!';
+    $scope.sortBy = "title";
+    $scope.searchByName = "";
+    $http.get('data/imdb250.json').success(function(data) {
+        $scope.movies = data;
+    }).error(function (){
+        $scope.error = "Cannot load movies";
+    });
+}]);
