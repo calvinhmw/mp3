0b0bd7773b4c8c11840bd038a0fdad9756e724aa
Mingwei Hu <calvinmwhu@gmail.com>
2016-03-06 23:13:28 -0600

    yes

--- source_js/app.js
+++ source_js/app.js
@@ -1,11 +1,22 @@
-var app = angular.module('mp3',['ngRoute']);
+var app = angular.module('mp3', ['ngRoute']);
 
 app.config(function ($routeProvider) {
     $routeProvider
-        .when('/list', {
+        .when('/movies', {
             templateUrl: 'partials/list.html',
             controller: 'ListViewController'
-        }).otherwise({
-       redirectTo: '/list'
+        }).when('/detail/:rank', {
+        templateUrl: 'partials/details.html',
+        controller: 'DetailViewController'
+    }).when('/movies/gallery', {
+        templateUrl: 'partials/gallery.html',
+        controller: 'GalleryViewController'
+    }).otherwise({
+        redirectTo: '/movies'
     });
 });
+
+
+app.factory('httpService', ['$http', function($http){
+
+}]);
--- source_js/controllers.js
+++ source_js/controllers.js
@@ -1,10 +1,59 @@
-app.controller('ListViewController', ['$scope', '$http', function ($scope, $http) {
+app.controller('ListViewController', ['$scope', '$http', '$location', function ($scope, $http, $location) {
     $scope.header = 'Search for a movie here!';
     $scope.sortBy = "title";
+    $scope.reverse = false;
     $scope.searchByName = "";
-    $http.get('data/imdb250.json').success(function(data) {
+    $http.get('data/imdb250.json').success(function (data) {
         $scope.movies = data;
-    }).error(function (){
+    }).error(function () {
+        $scope.error = "Cannot load movies";
+    });
+    $scope.goToDetail = function (rank) {
+        $location.path('/detail/' + rank);
+    }
+}]);
+
+
+app.controller('DetailViewController', ['$scope', '$http', '$routeParams', '$location', function ($scope, $http, $routeParams, $location) {
+    $scope.rank = parseInt($routeParams.rank);
+    $http.get('data/imdb250.json').success(function (data) {
+        $scope.movies = data;
+        $scope.numMovies = parseInt($scope.movies.length);
+        $scope.next = ($scope.rank + 1) % $scope.numMovies;
+        $scope.pre = ($scope.rank - 1 > 0) ? $scope.rank - 1 : $scope.numMovies;
+        $scope.movie = $scope.movies[$scope.rank - 1];
+    }).error(function () {
+        $scope.error = "Cannot load movies";
+    });
+}]);
+
+app.controller('GalleryViewController', ['$scope', '$http', '$location', function ($scope, $http, $location) {
+    $http.get('data/imdb250.json').success(function (data) {
+        $scope.movies = data;
+        $scope.selectedMovies = $scope.movies;
+        $scope.genres = ['All'];
+        for (var i = 0; i < $scope.movies.length; i++) {
+            var movie = $scope.movies[i];
+            for (var j = 0; j < movie.genre.length; j++) {
+                if ($scope.genres.indexOf(movie.genre[j]) == -1) {
+                    $scope.genres.push(movie.genre[j]);
+                }
+            }
+        }
+        $scope.filterByGenre = function (genre) {
+            if (genre == 'All') {
+                $scope.selectedMovies = $scope.movies;
+            } else {
+                $scope.selectedMovies = $scope.movies.filter(function (m) {
+                    return m.genre.indexOf(genre) != -1;
+                });
+            }
+            console.log($scope.selectedMovies);
+        }
+        $scope.goToDetail = function (rank) {
+            $location.path('/detail/' + rank);
+        }
+    }).error(function () {
         $scope.error = "Cannot load movies";
     });
 }]);
